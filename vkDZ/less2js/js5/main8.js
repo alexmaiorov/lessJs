ad48f0f2dfbaf6dc6fe6fa89c92ad0334f61105c
VicKonst21 <vickonstantinov92@gmail.com>
2020-10-07 23:55:41 +0300

    VKdz

---
+++ vkDZ/less2js/js5/main8.js
@@ -0,0 +1,19 @@
+let m = 999;
+let obj = {
+    num: prompt('Enter number'),
+    units: 0,
+    tens: 0,
+    hundreds: 0,
+};
+
+if (obj.num < 10) {
+    obj.units = obj.num;
+} else if (obj.num <= 999) {
+    obj.units = obj.num % 10;
+    obj.tens = Math.floor(obj.num / 10 % 10);
+    obj.hundreds = Math.floor(obj.num / 100);
+} else {
+    obj.num = 0; 
+    console.log('Error number');
+}
+console.log(obj);
